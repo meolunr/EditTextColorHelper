041f3ced60dc5253cae0d271b2d2fd2df3b9731b
meolunr <meolunr@gmail.com>
2017-02-12 18:25:57 +0800

    Sample：Add Buttons

--- sample/src/main/java/me/iacn/editcolorhelper/sample/MainActivity.java
+++ sample/src/main/java/me/iacn/editcolorhelper/sample/MainActivity.java
@@ -1,11 +1,7 @@
 package me.iacn.editcolorhelper.sample;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.EditText;
-
-import me.iacn.editcolorhelper.EditColorHelper;
 
 public class MainActivity extends Activity {
 
@@ -13,8 +9,5 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        EditText etTest = (EditText) findViewById(R.id.et_test);
-        EditColorHelper.setColor(etTest, Color.parseColor("#03A9F4"));
     }
 }
