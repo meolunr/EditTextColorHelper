9ef1ab145de1c8d6b6612d58b909f05a553b93e3
meolunr <meolunr@gmail.com>
2017-02-12 16:20:34 +0800

    Sample：Used EditTextHelper

--- sample/src/main/java/me/iacn/editcolorhelpersample/MainActivity.java
+++ sample/src/main/java/me/iacn/editcolorhelpersample/MainActivity.java
@@ -1,7 +1,11 @@
 package me.iacn.editcolorhelpersample;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import me.iacn.editcolorhelper.EditColorHelper;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText etTest = (EditText) findViewById(R.id.et_test);
+        EditColorHelper.setColor(etTest, Color.parseColor("#03A9F4"));
     }
 }
